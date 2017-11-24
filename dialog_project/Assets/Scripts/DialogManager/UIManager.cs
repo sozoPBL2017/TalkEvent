54c576438e202715d6553c119f557a6a888fb3db
negamay <negamay1026@yahoo.co.jp>
2017-11-24 16:31:28 +0900

    second commit with tutorial

--- dialog_project/Assets/Scripts/DialogManager/UIManager.cs
+++ dialog_project/Assets/Scripts/DialogManager/UIManager.cs
@@ -9,36 +9,58 @@ public class UIManager: MonoBehaviour {
     public GameObject container_NPC;
     public GameObject container_PLAYER;
     public Text text_NPC;
-    public Text[] text_choices;
+    public Text[] text_Choices;
 
     void Start(){
-        
+        container_NPC.SetActive(false);
+        container_PLAYER.SetActive(false);
     }
 
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.Return)){
-            Begin();
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Return)){
+            if(!VD.isActive){
+                Begin();
+            }
+            else{
+                VD.Next();
+            }
         }
     }
 
 
-    void Begin () {
+    void Begin(){
         VD.OnNodeChange += UpdateUI;
         VD.OnEnd += End;
         VD.BeginDialogue(GetComponent<VIDE_Assign>());
 	}
 	
-	void UpdateUI (VD.NodeData data) {
-		
+	void UpdateUI(VD.NodeData data){
+        container_NPC.SetActive(false);
+        container_PLAYER.SetActive(false);
+        if (data.isPlayer){
+            container_PLAYER.SetActive(true);
+            for(int i=0;i < text_Choices.Length; i++){
+                if(i < data.comments.Length){
+                    text_Choices[i].transform.parent.gameObject.SetActive(true);
+                    text_Choices[i].text = data.comments[i];
+                }
+            }
+        }
+        else{
+            container_NPC.SetActive(true);
+            text_NPC.text = data.comments[data.commentIndex];
+        }
 	}
 
     void End(VD.NodeData data){
+        container_NPC.SetActive(false);
+        container_PLAYER.SetActive(false);
         VD.OnNodeChange -= UpdateUI;
         VD.OnEnd -= End;
         VD.EndDialogue();
     }
 
-    private void OnDisable() {
+    private void OnDisable(){
         if (container_NPC != null)
             End(null);
     }
